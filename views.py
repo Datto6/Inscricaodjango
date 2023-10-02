d69101e9c0aa576442ae73a67efaac368456d342
Datto6 <61701645+Datto6@users.noreply.github.com>
2023-10-02 18:41:52 -0300

    relacao entre users e inscricao terminada

--- views.py
+++ views.py
@@ -7,18 +7,19 @@ from django.db.models import Max
 def index(request):
     return render(request, "aulas/index.html")
 
-def inscricao(request,aluno_id):
-    aluno=Aluno.objects.get(pk=aluno_id).all()
+def inscricao(request):
+    aluno_id=int(request.user.username)
+    aluno=Aluno.objects.get(matricula=aluno_id)
     horario_max=Horario.objects.aggregate(Max('numero'))
     return render(request,"aulas/inscricao.html",{
         "aluno":aluno,
         "numero_horarios":horario_max,
-        "aulas":Aula.objects.exclude(vagas<=0).all(),
+        "aulas":Aula.objects.exclude(vagas=0).all(),
     })
 
-def form(request,aluno_id):
+def form(request):
     if request.method=="POST":
-        aluno=aluno.objects.get(pk=aluno_id)
+        aluno=aluno.objects.get(matricula=aluno_id)
         aula=Aula.object.get(pk=int(request.POST["aula"]))
         aula.alunos.add(aluno)
         return HttpResponseRedirect(reverse("perfil",args=(aluno_id,)))
